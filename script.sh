6efbc530f91b70c5b2cc2f3a9a5e3836d02d7683
Sandro William Pereira <44730545+sandrowp@users.noreply.github.com>
2022-11-27 08:21:46 -0300

    Corrige comando kubeclt

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ docker push sandrowp/proj01-backend:1.0
 docker push sandrowp/proj01-dabase:1.0
 
 echo "Criando serviços do cluster Kubernetes..."
-kubctl apply -f ./services.yml
+kubectl apply -f ./services.yml
 
 echo "Realizando os deployments..."
-kubctl apply -f ./deployment.yml
+kubectl apply -f ./deployment.yml
